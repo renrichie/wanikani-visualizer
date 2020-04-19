c08d0a1a00b1aa6f1b7455cf943b481c490ebc7b
Richie Ren <richie.ren95@gmail.com>
2020-04-18 20:41:21 -0500

    Added HTTP error handling; Refactored requests to be more generic and easily used for other REST calls

--- scraper.py
+++ scraper.py
@@ -5,30 +5,28 @@ API_URI = 'https://api.wanikani.com/v2/'
 
 class Scraper:
     def __init__(self, api_key):
-        self.__auth_header = auth_header = {
+        self.__auth_header = {
             'Authorization': f'Bearer {api_key}'
         }
 
-    def getUser(self):
-        user = requests.get(API_URI + 'user', headers=self.__auth_header)
+    def _perform_request(self, method: str, endpoint: str, headers: dict = None, params: dict = None, body: dict = None):
+        response = requests.request(method=method, url=endpoint, headers=headers, params=params, json=body)
+        response.raise_for_status()
 
-        # TODO: Error handling.
+        return response.json()
 
-        user = user.json()
+    def get_user(self):
+        user = self._perform_request(method='GET', endpoint=API_URI + 'user', headers=self.__auth_header)
         username = user['data']['username']
         level = user['data']['level']
 
         print(f'Username: {username}\nLevel: {level}\n')
 
-    def getAllLevelProgressions(self):
+    def get_all_level_progressions(self):
         print('======== LEVEL PROGRESSION DATA ========')
         # TODO: Handle pagination using pages['next_url'] even though it's set to 500 per page and there are only 60 levels.
         #       The pagination amount could change in the future.
-        level_progressions = requests.get(API_URI + 'level_progressions', headers=self.__auth_header)
-
-        # TODO: Error handling.
-
-        level_progressions = level_progressions.json()
+        level_progressions = self._perform_request(method='GET', endpoint=API_URI + 'level_progressions', headers=self.__auth_header)
 
         for level_prog in level_progressions['data']:
             level = level_prog['data']['level']
@@ -37,14 +35,9 @@ class Scraper:
             end_date = level_prog['data']['completed_at'] or 'N/A'
             print(f'Level: {level} | Start date: {start_date} | Pass date: {pass_date} | Completion date: {end_date}')
 
-    def getAllAssignments(self):
+    def get_all_assignments(self):
         print('\n======== ASSIGNMENT PROGRESS DATA ========')
-
-        assignments = requests.get(API_URI + 'assignments', headers=self.__auth_header)
-
-        # TODO: Error handling.
-
-        assignments = assignments.json()
+        assignments = self._perform_request(method='GET', endpoint=API_URI + 'assignments', headers=self.__auth_header)
 
         # TODO: Handle pagination.
         for assignment in assignments['data']:
@@ -65,12 +58,11 @@ if __name__ == '__main__':
                 api_key = json.load(file)['api_key']
                 print('API key loaded.\n')
             except Exception as e:
-                print('An error occurred while loading the JSON: {}\n'.format(str(e)))
+                raise SystemExit(f'An error occurred while loading the JSON: {e}')
     except IOError:
-        print('Unable to load the API key.\n')
-        raise SystemExit
+        raise SystemExit('Unable to load the API key.')
 
     scraper = Scraper(api_key)
-    scraper.getUser()
-    scraper.getAllLevelProgressions()
-    scraper.getAllAssignments()
+    scraper.get_user()
+    scraper.get_all_level_progressions()
+    scraper.get_all_assignments()
