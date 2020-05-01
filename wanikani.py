de2c2bdb1dc45dc834099e634ed8b6617ef8212d
Richie Ren <richie.ren95@gmail.com>
2020-05-01 17:01:46 -0500

    Added date format constant; Added documentation

--- wanikani.py
+++ wanikani.py
@@ -1,7 +1,19 @@
 import requests
 
+# The ISO-8601 datetime format used by WaniKani.
+DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
+
 
 class WaniKaniClient:
+    """
+    This is a simple WaniKani client to get data from the API such as
+    user info, level progression, assignments, and subjects.
+
+    Parameters
+    ----------
+    api_key : str
+        The API key to be used to query for info - preferably read-only.
+    """
     API_URI = 'https://api.wanikani.com/v2/'
 
     def __init__(self, api_key):
@@ -9,7 +21,21 @@ class WaniKaniClient:
             'Authorization': f'Bearer {api_key}'
         }
 
-    def _perform_paginated_get_request(self, endpoint):
+    def _perform_paginated_get_request(self, endpoint: str):
+        """
+        A generator for generic GET requests that automatically handles pagination for the user.
+
+        Parameters
+        ----------
+        endpoint : str
+            The full endpoint URI to send the GET request to.
+
+        Returns
+        -------
+        dict
+            The JSON response for the current page.
+
+        """
         session = requests.Session()
         response = session.get(url=endpoint, headers=self.__auth_header)
         response.raise_for_status()
@@ -24,6 +50,15 @@ class WaniKaniClient:
             yield page
 
     def get_user(self):
+        """
+        Gets the user info.
+
+        Returns
+        -------
+        dict
+            A JSON response containing the user info.
+
+        """
         response = requests.get(url=WaniKaniClient.API_URI + 'user', headers=self.__auth_header)
         response.raise_for_status()
 
@@ -33,11 +68,40 @@ class WaniKaniClient:
 
         print(f'Username: {username}\nLevel: {level}\n')
 
+        return user
+
     def get_level_progressions(self):
+        """
+        A generator for getting all the level progression info.
+
+        Returns
+        -------
+        dict
+            The JSON response for the current page of level progression info.
+
+        """
         return self._perform_paginated_get_request(endpoint=WaniKaniClient.API_URI + 'level_progressions')
 
     def get_assignments(self):
+        """
+        A generator for getting all the assignment info.
+
+        Returns
+        -------
+        dict
+            The JSON response for the current page of assignment info.
+
+        """
         return self._perform_paginated_get_request(endpoint=WaniKaniClient.API_URI + 'assignments')
 
     def get_subjects(self):
+        """
+        A generator for getting all the subject info.
+
+        Returns
+        -------
+        dict
+            The JSON response for the current page of subject info.
+
+        """
         return self._perform_paginated_get_request(endpoint=WaniKaniClient.API_URI + 'subjects')
