090f66e837a86eebeb0c119b1e1e8a8040fa1c17
Richie Ren <richie.ren95@gmail.com>
2020-05-01 17:02:51 -0500

    Level progression processing now calculates mean and median durations

--- analyzer.py
+++ analyzer.py
@@ -1,18 +1,87 @@
 import json
+from datetime import datetime
 
-from wanikani import WaniKaniClient
+import wanikani
+import stats
 
 
 class Analyzer:
-    def process_level_progressions(self, progressions):
+    def _get_aggregates(self, data: list):
+        """
+        Creates a dictionary containing the mean and median aggregates for the list.
+        If the list is empty, the value for each key will be None.
+
+        Parameters
+        ----------
+        data : list
+            A list of numeric values.
+
+        Returns
+        -------
+        dict
+            A dictionary with the 'mean' and 'median' keys containing their respective values.
+
+        """
+        return {'mean': stats.get_mean(data), 'median': stats.get_median(data)}
+
+    def _calculate_time_delta(self, first_date: str, second_date: str):
+        """
+        Calculates the time delta between two dates in terms of number of seconds.
+        The order of the strings does not matter.
+
+        Parameters
+        ----------
+        first_date : str
+            A datetime string provided by the WaniKani API, which is in ISO-8601.
+        second_date : str
+            A datetime string provided by the WaniKani API, which is in ISO-8601.
+
+        Returns
+        -------
+        int
+            The number of seconds elapsed between the two datetimes.
+
+        """
+        start = datetime.strptime(first_date, wanikani.DATE_FORMAT) if first_date != 'N/A' else None
+        end = datetime.strptime(second_date, wanikani.DATE_FORMAT) if second_date != 'N/A' else None
+        delta = abs((end - start).total_seconds()) if start is not None and end is not None else None
+
+        return delta
+
+    def process_level_progressions(self, progressions: dict):
+        level_stats = {}
+        durations = {
+            'pass': [],
+            'complete': []
+        }
+
         for level_prog in progressions['data']:
             level = level_prog['data']['level']
             start_date = level_prog['data']['started_at'] or 'N/A'
             pass_date = level_prog['data']['passed_at'] or 'N/A'
             end_date = level_prog['data']['completed_at'] or 'N/A'
+
+            level_stats[level] = {
+                'pass_duration': self._calculate_time_delta(start_date, pass_date),
+                'complete_duration':  self._calculate_time_delta(start_date, end_date)
+            }
+
+            if level_stats[level]['pass_duration'] is not None:
+                durations['pass'].append(level_stats[level]['pass_duration'])
+
+            if level_stats[level]['complete_duration'] is not None:
+                durations['complete'].append(level_stats[level]['complete_duration'])
+
             print(f'Level: {level:>2} | Start date: {start_date:>27} | Pass date: {pass_date:>27} | Completion date: {end_date:>27}')
 
-    def process_assignments(self, assignments, subjects):
+        level_stats['aggregates'] = {
+            'pass': self._get_aggregates(durations['pass']),
+            'complete': self._get_aggregates(durations['complete'])
+        }
+
+        return level_stats
+
+    def process_assignments(self, assignments: dict, subjects: dict):
         for assignment in assignments['data']:
             # TODO: Query reviews for more data too.
             subject_id = assignment['data']['subject_id']
@@ -47,7 +116,7 @@ if __name__ == '__main__':
     except IOError:
         raise SystemExit('Unable to load the API key.')
 
-    client = WaniKaniClient(api_key)
+    client = wanikani.WaniKaniClient(api_key)
     client.get_user()
 
     analyzer = Analyzer()
