235f7699e703d29bd9946a7a37795761da90ca97
Richie Ren <richie.ren95@gmail.com>
2020-06-07 23:06:48 -0500

    Added page for overall stats

--- app/routes.py
+++ app/routes.py
@@ -12,17 +12,15 @@ def index():
     form = AuthenticationForm()
 
     if form.validate_on_submit():
-        flash('Analyzing your data!')
-
         client = WaniKaniClient(form.api_key.data)
         db = PostgresClient(dbname='postgres', user='postgres', password='postgres')
         analyzer = Analyzer(wanikani=client, db=db)
-        flash(analyzer.analyze_user_info())
+        info = analyzer.analyze_user_info()
 
         form.api_key.data = ''
 
         # Just re-render everything at the base URL to circumvent people visiting a separate stats page
         # before they even start an analysis.
-        return render_template('stats.html', logo=app.config['LOGO'])
+        return render_template('overall_stats.html', title='Overall Stats', profile_pic=app.config['LOGO'], data=info)
 
-    return render_template('index.html', form=form, logo=app.config['LOGO'])
+    return render_template('index.html', title='Home', form=form, logo=app.config['LOGO'])
